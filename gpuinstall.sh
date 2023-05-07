1b33a7680693ab4c2eb07f05ebcc13291a18e2ad
jz <jiangzhou.jz@alibaba-inc.com>
2023-05-07 08:59:04 +0800

    gpu

---
+++ gpuinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#这一行是表示使用 /bin/bash 作为脚本的解释器
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
+sudo systemctl start docker
+
+docker pull registry.cn-shanghai.aliyuncs.com/tcc_public/aigc:stable
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+
+yum install -y nvidia-container-toolkit
+
+#GPU启动命令：
+docker run --gpus all --network host registry.cn-shanghai.aliyuncs.com/tcc_public/aigc:stable
